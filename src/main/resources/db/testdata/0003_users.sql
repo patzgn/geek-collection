8c2aaa3d79112db0181454594b64040b84f74e87
patzgn <103074854+patzgn@users.noreply.github.com>
2023-03-05 13:06:01 +0100

    add user

---
+++ src/main/resources/db/testdata/0003_users.sql
@@ -0,0 +1,5 @@
+insert into users (email, password) values ('laizkovitch0@is.gd', 'wA8JPik');
+insert into users (email, password) values ('zdedenham1@msu.edu', '5Zso7nE');
+insert into users (email, password) values ('ddmitrovic2@cpanel.net', 'TXXTfMUVXw');
+insert into users (email, password) values ('cwhitney3@cmu.edu', 'rSn6PXSgbf');
+insert into users (email, password) values ('ddemko4@netvibes.com', 'dgDYcAjby');
